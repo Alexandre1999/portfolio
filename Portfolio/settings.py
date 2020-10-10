a31d13e2e488d73cd87698041bb8ae73471f3d3a
Ivan <alexandre.raymond@etu.u-pec.fr>
2020-10-10 23:29:52 +0200

    mod static dir

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -142,8 +142,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
